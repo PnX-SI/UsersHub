5cc22968bed52308e09355e20609857a85d262bc
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2017-12-04 12:20:25 +0100

    SQL - Ajout d'une vue

--- data/usershub.sql
+++ data/usershub.sql
@@ -513,6 +513,78 @@ $$;
 DO
 $$
 BEGIN
+CREATE OR REPLACE VIEW v_userslist_forall_applications AS 
+ SELECT a.groupe,
+ a.id_role,
+ a.identifiant,
+ a.nom_role,
+ a.prenom_role,
+ a.desc_role,
+ a.pass,
+ a.email,
+ a.id_organisme,
+ a.organisme,
+ a.id_unite,
+ a.remarques,
+ a.pn,
+ a.session_appli,
+ a.date_insert,
+ a.date_update,
+ max(a.id_droit) AS id_droit_max,
+ a.id_application
+ FROM ( SELECT u.groupe,
+ u.id_role,
+ u.identifiant,
+ u.nom_role,
+ u.prenom_role,
+ u.desc_role,
+ u.pass,
+ u.email,
+ u.id_organisme,
+ u.organisme,
+ u.id_unite,
+ u.remarques,
+ u.pn,
+ u.session_appli,
+ u.date_insert,
+ u.date_update,
+ c.id_droit,
+ c.id_application
+ FROM t_roles u
+ JOIN cor_role_droit_application c ON c.id_role = u.id_role
+ WHERE u.groupe = false
+ UNION
+ SELECT u.groupe,
+ u.id_role,
+ u.identifiant,
+ u.nom_role,
+ u.prenom_role,
+ u.desc_role,
+ u.pass,
+ u.email,
+ u.id_organisme,
+ u.organisme,
+ u.id_unite,
+ u.remarques,
+ u.pn,
+ u.session_appli,
+ u.date_insert,
+ u.date_update,
+ c.id_droit,
+ c.id_application
+ FROM t_roles u
+ JOIN cor_roles g ON g.id_role_utilisateur = u.id_role
+ JOIN cor_role_droit_application c ON c.id_role = g.id_role_groupe
+ WHERE u.groupe = false) a
+ GROUP BY a.groupe, a.id_role, a.identifiant, a.nom_role, a.prenom_role, a.desc_role, a.pass, a.email, a.id_organisme, a.organisme, a.id_unite, a.remarques, a.pn, a.session_appli, a.date_insert, a.date_update, a.id_application;
+EXCEPTION WHEN duplicate_object  THEN
+    -- do nothing, it's already there
+END
+$$;
+
+DO
+$$
+BEGIN
 INSERT INTO bib_droits (id_droit, nom_droit, desc_droit) VALUES (5, 'validateur', 'Il valide bien sur');
 INSERT INTO bib_droits (id_droit, nom_droit, desc_droit) VALUES (4, 'modérateur', 'Peu utilisé');
 INSERT INTO bib_droits (id_droit, nom_droit, desc_droit) VALUES (0, 'aucun', 'aucun droit.');
