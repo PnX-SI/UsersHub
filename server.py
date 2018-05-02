4f23afd9b237047172ac82b51d870a5ad28b67f5
Laumond11u <gab.gabin88@gmail.com>
2018-05-02 12:23:35 +0200

    ajout fichier de config

---
+++ server.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from userhub.env import db
+import config
+
+
+
+app = Flask(__name__, template_folder= "userhub/templates" )
+
+app.secret_key = config.SECRET_KEY
+app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = config.SQLALCHEMY_TRACK_MODIFICATIONS
+db.init_app(app)
+with app.app_context():
+
+    from userhub.t_roles import route
+    app.register_blueprint(route.route,  url_prefix='/t_roles')
+
+    from userhub.bib_organismes import route
+    app.register_blueprint(route.route,  url_prefix='/bib_organismes')
+
+    from userhub.groupe import route
+    app.register_blueprint(route.route,  url_prefix='/groupe')
+
+    from userhub.t_applications import route
+    app.register_blueprint(route.route, url_prefix='/application')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
