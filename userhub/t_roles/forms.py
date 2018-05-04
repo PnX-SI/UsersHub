ee5b24c35055eb99221273eaee6c00ad5f6e41b2
Laumond11u <gab.gabin88@gmail.com>
2018-05-04 15:54:12 +0200

    maj requierment

--- userhub/t_roles/forms.py
+++ userhub/t_roles/forms.py
@@ -1,8 +1,15 @@
 
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, HiddenField, SelectField, RadioField,SelectMultipleField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, HiddenField, SelectField, RadioField,SelectMultipleField, widgets, Form
+from wtforms.validators import DataRequired, Email
+
+
+class MultiCheckboxField(SelectMultipleField):
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
+
+
 
 
 
@@ -11,18 +18,17 @@ class Utilisateur(FlaskForm):
     nom_role =  StringField('Nom', validators=[DataRequired()])
     prenom_role = StringField( 'Prenom', validators=[DataRequired()])
     id_organisme = SelectField('Choix Organisme',coerce=int ,choices = [], default = -1)
-    a_groupe = SelectMultipleField('Choix Groupe', choices = [], coerce=int)
+    a_groupe = MultiCheckboxField('Choix Groupe', choices = [], coerce=int)
     identifiant = StringField('Identifiant', validators=[DataRequired()])
     pass_plus = PasswordField('Password', validators= [DataRequired()])
     mdpconf = PasswordField('Confirmation', validators= [DataRequired()])
     desc_role = StringField('Description du role' , validators = [DataRequired()])
-    email = StringField('E-mail', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[Email()])
     groupe = HiddenField('groupe', default = None)
     remarques = StringField('Remarques', validators=[DataRequired()])
     id_role = HiddenField('id')
     submit = SubmitField('Envoyer')
-
-
+   
     
 
-    
+
