0b7783b1bb4c0f12738f950aa355b4f26c5fdbf4
Camille Monchicourt <camille.monchicourt@ecrins-parcnational.fr>
2018-05-11 11:24:35 +0200

    SQL update V1 to V2
    Début de script permettant de simuler les tables de la V1 en créant des vues du même nom, basées sur les tags.

---
+++ data/update_1to2.sql
@@ -0,0 +1,70 @@
+-- Ce script permet de recréer des tables qui simulent les tables de UsersHub V1 en se basant sur les tags
+-- pour que les anciennes applications continuent à fonctionner
+-- @TODO : ATTENTION, il manque les récupération des données avant de supprimer les anciennes tables
+
+-- Associer les tags de type Liste à l'application GeoNature
+INSERT INTO utilisateurs.cor_application_tag(id_application, id_tag) VALUES
+(14,100)
+,(14,101)
+,(14,102)
+;
+
+-- Supprimer la table t_menus
+DROP TABLE utilisateurs.t_menus;
+
+-- Vue recréant l'equivalent de t_menus
+CREATE OR REPLACE VIEW utilisateurs.t_menus AS 
+SELECT 
+ t.id_tag AS id_menu,
+ t.tag_name AS nom_menu,
+ t.tag_desc AS desc_menu,
+ c.id_application
+FROM utilisateurs.bib_tag_types b
+LEFT JOIN utilisateurs.t_tags t ON b.id_tag_type = t.id_tag_type
+LEFT JOIN utilisateurs.cor_application_tag c ON c.id_tag = t.id_tag
+WHERE b.id_tag_type = 4
+
+-- Supprimer la table cor_role_menu
+DROP TABLE utilisateurs.cor_role_menu;
+
+-- Vue recréant l'equivalent de cor_role_menu
+CREATE OR REPLACE VIEW utilisateurs.cor_role_menu AS 
+SELECT 
+ c.id_role,
+ c.id_tag AS id_menu
+FROM utilisateurs.cor_role_tag c
+JOIN utilisateurs.t_menus v ON v.id_menu = c.id_tag
+
+-- Associer des roles aux tags de type Droits V1
+INSERT INTO utilisateurs.cor_role_tag(id_role, id_tag) VALUES
+(20002,2)
+;
+
+-- Associer les tags de type Droits V1 à l'application GeoNature
+-- Si il n'y a pas d'application définie, alors le tag devrait s'appliquer à toutes les applications
+INSERT INTO utilisateurs.cor_application_tag(id_application, id_tag) VALUES
+(14,2)
+;
+
+-- Supprimer la table bib_droits
+DROP TABLE utilisateurs.bib_droits;
+
+-- Vue recréant l'equivalent de bib_droits
+CREATE OR REPLACE VIEW utilisateurs.bib_droits AS 
+SELECT 
+ t.id_tag AS id_droit,
+ t.tag_name AS nom_droit,
+ t.tag_desc AS desc_desc_droit
+FROM utilisateurs.bib_tag_types b
+JOIN utilisateurs.t_tags t ON b.id_tag_type = t.id_tag_type
+WHERE b.id_tag_type = 3
+
+-- Vue recréant l'equivalent de cor_role_droit_application
+CREATE OR REPLACE VIEW utilisateurs.cor_role_droit_application AS 
+SELECT 
+ c.id_role,
+ b.id_droit, 
+ ct.id_application
+FROM utilisateurs.cor_role_tag c
+JOIN utilisateurs.bib_droits b ON b.id_droit = c.id_tag
+JOIN utilisateurs.cor_application_tag ct ON ct.id_tag = c.id_tag
