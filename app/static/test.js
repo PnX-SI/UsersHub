7653d04b7874c3f3e17487fe8e5402083f2a20d9
Laumond11u <gab.gabin88@gmail.com>
2018-05-17 16:20:01 +0200

    transfert visuel users

--- app/static/test.js
+++ app/static/test.js
@@ -4,14 +4,13 @@ $( document ).ready(function() {
         var tab = []
         $('#user input[type="checkbox"]:checked').each(function(){
             var getRow = $(this).parents('tr');
-            var user ={
-                id : getRow.find('td:eq(1)').html(),
-                name : getRow.find('td:eq(2)').html()
-            }
-            tab.push(user);
-            addTab(tab)
+           
+            tab.push(getRow[0]);
+            
+            
         });
-        console.log(tab)
+        addTab(tab)
+ 
     });    
 
 
@@ -24,21 +23,23 @@ $( document ).ready(function() {
     };
 
     
+    function addTab(tab){
+    var table = $('#test')
+    console.log(table)
+    console.log(tab)
+    for(var i = 0; i<tab.length;i++){
+        console.log(tab[i])
+        
+        
+        table.append(tab[i]);
+    }    
     
+    }
     
 });
 
 
-function addTab(tab){
-    for(var i = 0; i<tab.length;i++){
-        var sHtml = "<tr>"+
-                "   <td class='m1'>"+tab[i].id+"</td>"+
-                "   <td class='m2'>"+tab[i].name+"</td>"+
-                "</tr>";
-    }    
-    
-    $("#test").append(sHtml);
-}
+
 
 
 var deleteRaw = function (path){        
@@ -46,3 +47,7 @@ var deleteRaw = function (path){
         if (c == true)
            window.location.href = path;  
     }
+
+
+//  var user ={
+    // id : getRow.find('td:eq(1)').html()}
