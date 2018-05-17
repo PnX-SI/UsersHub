f5ff7553320a977d65f4bd2cc294ac706dd8c21b
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2018-05-17 13:23:34 +0200

    install_db.sh n'est plus en root

--- install_db.sh
+++ install_db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Make sure only root can run our script
 if [ "$(id -u)" == "0" ]; then
-    echo "This script must be run as root" 1>&2
+    echo "This script must NOT be run as root" 1>&2
     exit 1
 fi
 
