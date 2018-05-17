bafdb2862d5979d0a0032663bc78bdd563be233a
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2018-05-17 12:30:18 +0200

    uniformisation des configurations  apache

--- install_app.sh
+++ install_app.sh
@@ -35,14 +35,13 @@ echo "        }" >> config/dbconnexions.json
 echo "    ]" >> config/dbconnexions.json
 echo "}" >> config/dbconnexions.json
 
-
+# configuration apache
 APACHE_REP=/var/www/
 if /usr/sbin/apache2 -v | grep -q version.*2.4; then
     echo apache 2.4
 	APACHE_REP=/var/www/html
-    echo 'Répertoire root de apache :' $APACHE_REP
 fi
-
-sudo ln -s ${PWD}/web/ $APACHE_REP/usershub
+echo 'Répertoire root de apache :' $APACHE_REP
+echo 'Pensez à faire votre configuration apache (voir la documentation).'
 
 echo "Fin. Vous devez manuellement éditer le fichier config/dbconnexoins.json et y ajouter les paramètres de connexions à toutes les bases que vous souhaitez synchroniser avec UsersHub"
