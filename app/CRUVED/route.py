1464a5b86ae8bc9904135c19ca20f020f1f63039
Laumond11u <gab.gabin88@gmail.com>
2018-05-28 17:05:49 +0200

    corrections erreurs ajout cruved

--- app/CRUVED/route.py
+++ app/CRUVED/route.py
@@ -21,7 +21,7 @@ def CRUVED():
     q = TRoles.get_all(as_model=True)
     data = TRoles.test_group([data.as_dict_full_name() for data in q.all()])
     if request.method == 'GET':
-        return render_template('CRUVED.html',fLine = fLine, line = columns,fLineCruved = fLineCruved, table = data, key = 'id_role', pathC = '/CRUVED/user/',pathU='' ,group = 'groupe',name_list = 'Liste d\'Utilisateurs et de Groupes')       
+        return render_template('CRUVED.html',fLine = fLine, line = columns,fLineCruved = fLineCruved, table = data, key = 'id_role', pathC = '/CRUVED/user/',pathU='',group = 'groupe',name_list = 'Liste d\'Utilisateurs et de Groupes')       
     if request.method =='POST':
         return redirect(url_for('cruved.cruved_one'))
 
@@ -39,10 +39,9 @@ def cruved_one(id_role):
     contents = get_cruved_one(id_role)
     if contents == []:
         role = TRoles.get_one(id_role,as_model = True).as_dict_full_name()
-        name = role['full_name']
     else:
-        name = contents[0]["full_name"]
-    return render_template('CRUVED.html',fLine = fLine, line = columns, table = data,fLineCruved = fLineCruved,lineCruved = columnsCruved,tableCruved=contents, key = 'id_role', pathC = '', pathU='' , group = 'groupe',name_list = 'Liste d\'Utilisateurs et de Groupes',name_role =name)       
+        role = contents[0]
+    return render_template('CRUVED.html',fLine = fLine, line = columns, table = data,fLineCruved = fLineCruved,lineCruved = columnsCruved,tableCruved=contents, key = 'id_role',id=role['id_role'],  pathC = '', pathU='',pathA= '/CRUVED/update/'  , group = 'groupe',name_list = 'Liste d\'Utilisateurs et de Groupes',name_role =role['full_name'])       
 
 # @route.route('CRUVED/add/new', defautls={'id_role':None,'id_application':None}, methods=['GET','POST'])
 # @route.route('CRUVED/update/<id_role>/<id_application>', methods=['GET','POST'])
@@ -97,22 +96,24 @@ def cruved_user(id_role, id_application):
         if request.method =='GET':
             form.full_name_role.process_data(id_role)
         if request.method == 'POST':
-            form_data = {"id_role":id_role,"id_application":form.data['app']}
-            form_scope = pops(form.data)
-            for scope in form_scope:
-               if form_scope[scope] != -1:
-                    if scope == "scopeCreate":
-                        CorAppPrivileges.post({"id_tag_action":11,"id_tag_object":form_scope[scope],**form_data})
-                    if scope == "scopeRead":
-                        CorAppPrivileges.post({"id_tag_action":12,"id_tag_object":form_scope[scope],**form_data})
-                    if scope == "scopeUpdate":
-                        CorAppPrivileges.post({"id_tag_action":13,"id_tag_object":form_scope[scope],**form_data})
-                    if scope == "scopeValidate":
-                        CorAppPrivileges.post({"id_tag_action":14,"id_tag_object":form_scope[scope],**form_data})
-                    if scope == "scopeExport":
-                        CorAppPrivileges.post({"id_tag_action":15,"id_tag_object":form_scope[scope],**form_data})
-                    if scope == "scopeDelete": 
-                        CorAppPrivileges.post({"id_tag_action":16,"id_tag_object":form_scope[scope],**form_data})
+            if form.Validate and form.validate_on_submit() :
+                form_data = {"id_role":id_role,"id_application":form.data['app']}
+                form_scope = pops(form.data)
+                for scope in form_scope:
+                    if form_scope[scope] != -1:
+                            if scope == "scopeCreate":
+                                CorAppPrivileges.post({"id_tag_action":11,"id_tag_object":form_scope[scope],**form_data})
+                            if scope == "scopeRead":
+                                CorAppPrivileges.post({"id_tag_action":12,"id_tag_object":form_scope[scope],**form_data})
+                            if scope == "scopeUpdate":
+                                CorAppPrivileges.post({"id_tag_action":13,"id_tag_object":form_scope[scope],**form_data})
+                            if scope == "scopeValidate":
+                                CorAppPrivileges.post({"id_tag_action":14,"id_tag_object":form_scope[scope],**form_data})
+                            if scope == "scopeExport":
+                                CorAppPrivileges.post({"id_tag_action":15,"id_tag_object":form_scope[scope],**form_data})
+                            if scope == "scopeDelete": 
+                                CorAppPrivileges.post({"id_tag_action":16,"id_tag_object":form_scope[scope],**form_data})
+            return redirect(url_for(('cruved.CRUVED')))
             print(form_data)
             print(form_scope)
             
