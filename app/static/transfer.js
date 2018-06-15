c53b84af8df57d4dc53ab39475f6c97adc504a18
Laumond11u <gab.gabin88@gmail.com>
2018-06-15 10:42:13 +0200

    correction droit application

--- app/static/transfer.js
+++ app/static/transfer.js
@@ -75,10 +75,12 @@ var add = function(app) {
 var del = function(app){
     var tab = []
     $('#adding_table input[type="checkbox"]:checked').each(function(){
+        var Row = $(this).parents('tr');
         if (app != null){
-            $('.right').remove()
+            Row.find('.right').remove()
+            
         }
-        var Row = $(this).parents('tr');        
+        var Row = $(this).parents('tr');
         tab.push(Row[0]);
         $("#adding_table").find("input[type=checkbox]:checked").prop('checked', false);
         var ID=$(this).parents('tr').find('td:eq(1)').html();
@@ -192,77 +194,7 @@ $( document ).ready(function() {
 
     
 
-    // $("#add").click(lala);
-    // $("#add").click(function(){
-    //     var tab = []
-    //     $('#user input[type="checkbox"]:checked').each(function(){
-    //         var Row = $(this).parents('tr').append(fill_select());
-    //         tab.push(Row[0]);
-    //         $("#user").find("input[type=checkbox]:checked").prop('checked', false);
-    //         var ID=$(this).parents('tr').find('td:eq(1)').html();
-    //         tab_add.push(ID);
-    //         if (isInTabb(tab_del,ID) == true){
-    //             tab_del.splice(tab_del.indexOf(ID),1);
-    //             tab_add.splice(tab_add.indexOf(ID),1);
-    //         }
-            
-
-
-    //     });
-        
-    //     var table = $('#adding_table')
-    //     addTab(tab,table)
- 
-    // });
     
-    // $("#delete").click(function(){
-    //     var tab = []
-    //     $('#adding_table input[type="checkbox"]:checked').each(function(){
-    //         $('.right').remove()
-    //         var Row = $(this).parents('tr');
-            
-    //         tab.push(Row[0]);
-    //         $("#adding_table").find("input[type=checkbox]:checked").prop('checked', false);
-    //         var ID=$(this).parents('tr').find('td:eq(1)').html();
-    //         tab_del.push(ID)
-    //         if (isInTabb(tab_add,ID) == true){
-    //             tab_add.splice(tab_add.indexOf(ID),1);
-    //             tab_del.splice(tab_del.indexOf(ID),1);
-    //         }
-            
-    //     });
-    //     var table = $('#user')
-    //     addTab(tab,table)
- 
-    // });
-
-    // function addTab(tab,table){
-    //     // table.find("input[type=checkbox]").attr('checked', false);
-    //     for(var i = 0; i<tab.length;i++){
-            
-    //         table.append(tab[i]);
-    //     }    
-        
-    // }
-
-    // $("#update").click(function(){
-    //     console.log("tableau d ajout : "+ tab_add);
-    //     console.log("tablea de suppression : "+ tab_del );
-    //     var data ={}
-    //     data["tab_add"] = tab_add;
-    //     data["tab_del"]= tab_del;
-
-    //     $.ajax({
-    //         url : $(location).attr('href'),
-    //         type : 'post',
-    //         data : JSON.stringify(data),
-    //         contentType:"application/json; charset=utf-8",
-    //         dataType:"json"
-    //     });
-
-    //    tab_add = []
-    //    tab_del = []
-    // });
 
 
     console.log('coucou');
@@ -276,40 +208,3 @@ $( document ).ready(function() {
    
 }); 
 
-// function isInTabb(tab,id){
-//     var bool = false
-//     tab.forEach(element => {
-//         if (element == id){
-//             bool = true
-//         }
-//     });
-//     return bool
-// }
-
-
-
-// tab.forEach(element => {
-    
-// });
-
-// tab_del = tab_del.filter(function(el) {
-//     return el =! id
-// });
-
-// new = array.map(el => {
-//     el +1
-// })
-
-
-
-
-
-// var deleteRaw = function (path){        
-//         var c = confirm("Etes vous sur de vouloir supprimer cet élement ? ");
-//         if (c == true)
-//            window.location.href = path;  
-//     }
-
-
-//  var user ={
-    // id : getRow.find('td:eq(1)').html()}
