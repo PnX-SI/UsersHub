293fdeed0fbdbcfe6e245de965a4784947e6e3f3
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2018-11-14 10:03:52 +0100

    SQL - rétrocompatibilité pour les application basées sur le MCD V1

--- data/update_1.3.1to2.sql
+++ data/update_1.3.1to2.sql
@@ -19,7 +19,7 @@ CREATE SCHEMA save;
 INSERT INTO utilisateurs.cor_application_tag (id_tag, id_application)
 SELECT t.id_tag, (SELECT id_application FROM utilisateurs.t_applications WHERE nom_application IN('occtax')) 
 FROM utilisateurs.t_tags t
-JOIN save.t_menus  m ON m.nom_menu = t.tag_name
+JOIN utilisateurs.t_menus  m ON m.nom_menu = t.tag_name
 WHERE id_tag_type = 4
 AND tag_name ILIKE 'observateurs occtax';
 
@@ -89,8 +89,9 @@ $$;
 ---------
 --MENUS--
 ---------
-ALTER TABLE utilisateurs.t_menus SET SCHEMA save;
 ALTER TABLE utilisateurs.cor_role_menu SET SCHEMA save;
+ALTER TABLE utilisateurs.t_menus SET SCHEMA save;
+
 --Récupérer les informations concernant les menus pour en faire des tags de type List
 DO
 $$
@@ -159,7 +160,8 @@ SELECT
  c.id_role,
  t.tag_code::integer AS id_menu
 FROM utilisateurs.cor_role_tag c
-JOIN utilisateurs.t_tags t ON t.id_tag = c.id_tag;
+JOIN utilisateurs.t_tags t ON t.id_tag = c.id_tag
+WHERE t.id_tag_type = 4;
 
 
 ----------
@@ -335,6 +337,71 @@ CREATE OR REPLACE VIEW utilisateurs.v_usersaction_forall_gn_modules AS
    FROM all_users_tags v
   WHERE v.max_tag_object_code = v.tag_object_code::text;
 
+DROP VIEW v_userslist_forall_menu;
+CREATE OR REPLACE VIEW v_userslist_forall_menu AS 
+SELECT a.groupe, a.id_role, a.identifiant, a.nom_role, a.prenom_role, (upper(a.nom_role::text) || ' '::text) || a.prenom_role::text AS nom_complet, a.desc_role, a.pass, a.pass_plus, a.email, a.id_organisme, a.organisme, a.id_unite, a.remarques, a.pn, a.session_appli, a.date_insert, a.date_update, a.id_menu
+FROM (  SELECT u.groupe, u.id_role, u.identifiant, u.nom_role, u.prenom_role, u.desc_role, u.pass, u.pass_plus, u.email, u.id_organisme, o.nom_organisme AS organisme, u.id_unite, u.remarques, u.pn, u.session_appli, u.date_insert, u.date_update, c.id_menu
+        FROM utilisateurs.t_roles u
+        JOIN utilisateurs.cor_role_menu c ON c.id_role = u.id_role
+        LEFT JOIN utilisateurs.bib_organismes o ON o.id_organisme = u.id_organisme
+        WHERE u.groupe = false
+UNION 
+        SELECT u.groupe, u.id_role, u.identifiant, u.nom_role, u.prenom_role, u.desc_role, u.pass, u.pass_plus, u.email, u.id_organisme, o.nom_organisme AS organisme, u.id_unite, u.remarques, u.pn, u.session_appli, u.date_insert, u.date_update, c.id_menu
+        FROM utilisateurs.t_roles u
+        JOIN utilisateurs.cor_roles g ON g.id_role_utilisateur = u.id_role
+        JOIN utilisateurs.cor_role_menu c ON c.id_role = g.id_role_groupe
+        LEFT JOIN utilisateurs.bib_organismes o ON o.id_organisme = u.id_organisme
+        WHERE u.groupe = false
+) a;
+
+DROP VIEW v_userslist_forall_applications;
+CREATE OR REPLACE VIEW v_userslist_forall_applications AS 
+SELECT a.groupe, a.id_role, a.identifiant, a.nom_role, a.prenom_role, a.desc_role, a.pass, a.pass_plus, a.email, a.id_organisme, a.organisme, a.id_unite, a.remarques, a.pn, a.session_appli, a.date_insert, a.date_update, max(a.id_droit) AS id_droit_max, a.id_application
+FROM    (       SELECT u.groupe, u.id_role, u.identifiant, u.nom_role, u.prenom_role, u.desc_role, u.pass, u.pass_plus, u.email, u.id_organisme, o.nom_organisme AS organisme, u.id_unite, u.remarques, u.pn, u.session_appli, u.date_insert, u.date_update, c.id_droit, c.id_application
+                FROM utilisateurs.t_roles u
+                JOIN utilisateurs.cor_role_droit_application c ON c.id_role = u.id_role
+                LEFT JOIN utilisateurs.bib_organismes o ON o.id_organisme = u.id_organisme
+                WHERE u.groupe = false
+        UNION 
+                SELECT u.groupe, u.id_role, u.identifiant, u.nom_role, u.prenom_role, u.desc_role, u.pass, u.pass_plus, u.email, u.id_organisme, o.nom_organisme AS organisme, u.id_unite, u.remarques, u.pn, u.session_appli, u.date_insert, u.date_update, c.id_droit, c.id_application
+                FROM utilisateurs.t_roles u
+                JOIN utilisateurs.cor_roles g ON g.id_role_utilisateur = u.id_role
+                JOIN utilisateurs.cor_role_droit_application c ON c.id_role = g.id_role_groupe
+                LEFT JOIN utilisateurs.bib_organismes o ON o.id_organisme = u.id_organisme
+                WHERE u.groupe = false
+        ) a
+GROUP BY a.groupe, a.id_role, a.identifiant, a.nom_role, a.prenom_role, a.desc_role, a.pass, a.pass_plus, a.email, a.id_organisme, a.organisme, a.id_unite, a.remarques, a.pn, a.session_appli, a.date_insert, a.date_update, a.id_application;
+
+--On essaie de recréer une vue qui n'existe pas dans toutes les bases
+--Si elle n'existe pas une erreur est levée est la création ne se fait pas.
+DO
+$$
+BEGIN
+DROP VIEW utilisateurs.v_nomade_obs;
+CREATE OR REPLACE VIEW utilisateurs.v_nomade_obs AS 
+SELECT DISTINCT r.id_role AS codeobs, (r.nom_role::text || ' '::text) || r.prenom_role::text AS nomprenom
+FROM utilisateurs.t_roles r
+WHERE (r.id_role IN ( 
+        SELECT DISTINCT cr.id_role_utilisateur
+        FROM utilisateurs.cor_roles cr
+        WHERE (cr.id_role_groupe IN ( 
+                SELECT id_role
+                FROM utilisateurs.cor_role_menu
+                WHERE id_menu = 5)
+              ) 
+        AND cr.id_role_utilisateur <> 999
+        ORDER BY cr.id_role_utilisateur)
+) 
+OR (r.id_role IN ( 
+        SELECT crm.id_role
+        FROM utilisateurs.cor_role_menu crm
+        JOIN utilisateurs.t_roles r ON r.id_role = crm.id_role AND crm.id_menu = 5 AND r.groupe = false)
+        )
+ORDER BY (r.nom_role::text || ' '::text) || r.prenom_role::text, r.id_role;
+EXCEPTION WHEN undefined_table  THEN
+        RAISE NOTICE 'Cet vue n''existe pas';
+END
+$$;
 
 -- Supprimer le champs organisme de t_roles (https://github.com/PnEcrins/UsersHub/issues/38)
 ALTER TABLE utilisateurs.t_roles DROP COLUMN organisme RESTRICT;
@@ -343,13 +410,33 @@ ALTER TABLE utilisateurs.t_roles DROP COLUMN organisme RESTRICT;
 --SAVE--
 --------
 --TODO AVANT  SUPPRESSION : 
---vérifier qu'il n'y pas besoin de cascade (vues liées ou autre)
---vérifier que le "SET SCHEMA save" n'a pas modifier les FROM et les JOIN sur les vues qui références ces anciennes tables :
---  v_userslist_forall_applications
---  v_userslist_forall_menu
+--recréer les vues de GN1
 --  contactfaune.v_nomade_observateurs_faune
 --  contactflore.v_nomade_observateurs_flore
 --  contactinv.v_nomade_observateurs_inv
+
+--Rechercher les vues qui doivent être réécrites car pointant sur les tables déplacées dans le schéma save
+SELECT dependent_ns.nspname as dependent_schema
+, dependent_view.relname as dependent_view 
+, source_ns.nspname as source_schema
+, source_table.relname as source_table
+, pg_attribute.attname as column_name
+FROM pg_depend 
+JOIN pg_rewrite ON pg_depend.objid = pg_rewrite.oid 
+JOIN pg_class as dependent_view ON pg_rewrite.ev_class = dependent_view.oid 
+JOIN pg_class as source_table ON pg_depend.refobjid = source_table.oid 
+JOIN pg_attribute ON pg_depend.refobjid = pg_attribute.attrelid 
+    AND pg_depend.refobjsubid = pg_attribute.attnum 
+JOIN pg_namespace dependent_ns ON dependent_ns.oid = dependent_view.relnamespace
+JOIN pg_namespace source_ns ON source_ns.oid = source_table.relnamespace
+WHERE 
+source_ns.nspname = 'save'
+AND source_table.relname IN('cor_role_menu', 't_menus', 'bib_droits', 'cor_role_droit_application')
+AND pg_attribute.attnum > 0 
+--AND pg_attribute.attname = 'my_column'
+ORDER BY 1,2;
+
+
 --TABLES SUPPRIMABLES (dans cet ordre). A la discretion de chacun
 -- Supprimer la table cor_role_menu
 --DROP TABLE save.cor_role_menu;
