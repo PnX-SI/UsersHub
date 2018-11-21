888312b7abe0940b405b2b73489000660fba7acc
Camille Monchicourt <camille.monchicourt@ecrins-parcnational.fr>
2018-11-21 14:59:06 +0100

    UH data - Majuscules aux profils

--- data/usershub-data.sql
+++ data/usershub-data.sql
@@ -13,13 +13,13 @@ INSERT INTO t_applications (id_application, code_application, nom_application, d
 SELECT pg_catalog.setval('t_applications_id_application_seq', (SELECT max(id_application)+1 FROM utilisateurs.t_applications), false);	
 
 INSERT INTO t_profils (id_profil, code_profil, nom_profil, desc_profil) VALUES
-(0, '0', 'aucun', 'Aucun droit')
-,(1, '1', 'utilisateur', 'Ne peut que consulter')
-,(2, '2', 'rédacteur', 'Il possède des droit d''écriture pour créer des enregistrements')
-,(3, '3', 'référent', 'Utilisateur ayant des droits complémentaires au rédacteur (par exemple exporter des données ou autre)')
-,(4, '4', 'modérateur', 'Peu utilisé')
-,(5, '5', 'validateur', 'Il valide bien sur')
-,(6, '6', 'administrateur', 'Il a tous les droits');
+(0, '0', 'Aucun', 'Aucun droit')
+,(1, '1', 'Utilisateur', 'Ne peut que consulter')
+,(2, '2', 'Rédacteur', 'Il possède des droit d''écriture pour créer des enregistrements')
+,(3, '3', 'Référent', 'Utilisateur ayant des droits complémentaires au rédacteur (par exemple exporter des données ou autre)')
+,(4, '4', 'Modérateur', 'Peu utilisé')
+,(5, '5', 'Validateur', 'Il valide bien sur')
+,(6, '6', 'Administrateur', 'Il a tous les droits');
 SELECT pg_catalog.setval('t_profils_id_profil_seq', (SELECT max(id_profil)+1 FROM utilisateurs.t_profils), false);
 
 INSERT INTO utilisateurs.cor_profil_for_app (id_profil, id_application) VALUES
