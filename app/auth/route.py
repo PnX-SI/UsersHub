09c5b48c449b596b50675322191644488eb37665
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2019-01-03 12:42:54 +0100

    Correction de la route de déconnexion

--- app/auth/route.py
+++ app/auth/route.py
@@ -2,6 +2,9 @@ from flask import (
     Flask, redirect, url_for, render_template,
     Blueprint, request, session, flash, current_app
 )
+
+import bcrypt
+
 from app import genericRepository
 from app.auth import forms as authforms
 from app.models import TRoles
@@ -10,7 +13,8 @@ from app.env import db
 from flask_bcrypt import (Bcrypt,
                           check_password_hash,
                           generate_password_hash)
-import bcrypt
+
+from config import config
 
 route = Blueprint('login', __name__)
 
@@ -18,3 +22,10 @@ route = Blueprint('login', __name__)
 @route.route('/', methods=['GET'])
 def auth():
     return render_template('login.html', id_app=current_app.config['ID_APP'])
+
+@route.route('/logout', methods=['GET'])
+def logout():
+    resp = redirect(url_for('login.auth'), code=302)
+    resp.delete_cookie('token')
+    resp.delete_cookie('session')
+    return resp
