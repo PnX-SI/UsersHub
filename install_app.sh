dc491ade846c157f14fe665baf23888472faea5b
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2020-02-11 16:11:38 +0100

    python3 pour creer virtualenv

--- install_app.sh
+++ install_app.sh
@@ -26,7 +26,7 @@ cd ..
 # Installation de l'environement python
 
 echo "Installation du virtual env..."
-python -m virtualenv -p /usr/bin/python3 venv
+python3 -m virtualenv -p /usr/bin/python3 venv
 source venv/bin/activate
 pip install -r requirements.txt
 deactivate
