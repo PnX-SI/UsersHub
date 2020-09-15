4d22aab0c902a0658dd5d6c2f9fd11a189c91279
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2020-09-15 12:21:46 +0200

    Fix data/update: use comment instead of /echo

--- data/update_2.1.2to2.1.3.sql
+++ data/update_2.1.2to2.1.3.sql
@@ -1,8 +1,7 @@
-\echo 'Update UsersHub DB schema to v2.1.3'
+-- Update UsersHub DB schema to v2.1.3
 BEGIN;
 
-\echo '----------------------------------------------------------------------------'
-\echo 'Add confirmation_url field to temp_users table'
+-- Add confirmation_url field to temp_users table
 ALTER TABLE utilisateurs.temp_users ADD confirmation_url varchar(250) NULL;
 
 COMMIT;
