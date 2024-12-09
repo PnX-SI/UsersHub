9b1f3536bb54794d597b25b0b3335d845df8ea90
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2024-12-09 17:04:26 +0100

    fix: add missing revision to force utilisateurs branch to update

---
+++ app/migrations/versions/20589df23d98_update_utilisateur_schema.py
@@ -0,0 +1,25 @@
+"""update utilisateur schema
+
+Revision ID: 20589df23d98
+Revises: 6ec215fe023e
+Create Date: 2024-12-09 17:01:54.589941
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "20589df23d98"
+down_revision = "6ec215fe023e"
+branch_labels = None
+depends_on = ("b7c98935d9e8",)
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
